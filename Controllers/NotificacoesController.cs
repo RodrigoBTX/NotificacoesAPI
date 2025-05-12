a8071f47ca2e7250b66d8540d945be2262452a91
RodrigoBTX <rgoncalves@basetx.com>
2025-05-12 12:39:23 +0100

    Add project files.

---
+++ Controllers/NotificacoesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotificacoesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotificacoesController : ControllerBase
+    {
+        [HttpGet("novas")]
+        public IActionResult GetNovidades()
+        {
+            var count = DbHelper.ObterNovidades();
+            return Ok(new { novasTarefas = count });
+        }
+    }
+}
---
+++ DbHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+
+
+namespace NotificacoesAPI
+{
+    public static class DbHelper
+    {
+        private static readonly string connString = @"Server=BTX036\SQLEXP;Database=MaisRitmo33;User Id=sa;Password=\1qazxsw;";
+
+        public static int ObterNovidades()
+        {
+            using var conn = new SqlConnection(connString);
+            using var cmd = new SqlCommand("SELECT COUNT(*) FROM st (nolock) WHERE stns = 0", conn);
+            conn.Open();
+            return (int)cmd.ExecuteScalar();
+        }
+    }
+}
