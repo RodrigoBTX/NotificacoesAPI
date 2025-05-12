67652ac5476f76e660765ffdc58bfed1237d8be3
RodrigoBTX <rgoncalves@basetx.com>
2025-05-12 14:35:42 +0100

    added endpoint to test connection

---
+++ Controllers/TestDbController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotificacoesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestDbController : ControllerBase
+    {
+        [HttpGet("novidades")]
+        public IActionResult GetNovidades()
+        {
+            try
+            {
+                int count = DbHelper.ObterNovidades();
+                return Ok(new { TotalNovidades = count });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Erro: {ex.Message}");
+            }
+        }
+    }
+}
